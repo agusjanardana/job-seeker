7e0dfef6bc51ee09700224444659f9d410c95074
Agus Janardana <bjanardana@gmail.com>
2021-08-21 17:15:19 +0800

    fixing some problem

--- controller/authController.js
+++ controller/authController.js
@@ -2,55 +2,54 @@ const User = require('../models/User');
 
 //error handling
 const handleErrors = (err) => {
-   console.log(err.message, err.code);
-   let errors = { email: '', password: '' };
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
 
-   // duplicate email error
-   if (err.code === 11000) {
-      errors.email = 'email sudah teregistrasi';
-      return errors;
-   }
+    // duplicate email error
+    if (err.code === 11000) {
+        errors.email = 'email sudah teregistrasi';
+        return errors;
+    }
 
-   // validation errors
-   if (err.message.includes('user validation failed')) {
-      // console.log(err);
-      Object.values(err.errors).forEach(({ properties }) => {
-         // console.log(val);
-         // console.log(properties);
-         errors[properties.path] = properties.message;
-      });
-   }
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        // console.log(err);
+        Object.values(err.errors).forEach(({ properties }) => {
+            // console.log(val);
+            // console.log(properties);
+            errors[properties.path] = properties.message;
+        });
+    }
 
-   return errors;
+    return errors;
 };
 
 exports.register_get = (req, res) => {
-   res.render('partials/register');
+    res.render('auth/register');
 };
 
 exports.login_get = (req, res) => {
-   res.render('partials/login');
+    res.render('auth/login');
 };
 
 exports.register_post = async (req, res) => {
-   const { email, password } = req.body;
+    const { email, password, name } = req.body;
 
-   if (!req.body) {
-      res.status(400).send({
-         message: 'Ada field yang kosong',
-      });
-   }
-
-   try {
-      const user = await User.create({ email, password });
-      res.status(200).json(user);
-   } catch (err) {
-      const errors = handleErrors(err);
-      res.status(400).json({ errors });
-   }
+    // if (!req.body) {
+    //    res.status(400).send({
+    //       message: 'Ada field yang kosong',
+    //    });
+    // }
+    try {
+        const user = await User.create({ email, password, name });
+        res.status(200).json(user);
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
 };
 
 exports.login_post = (req, res) => {
-   const { email, password } = req.body;
-   res.send('login success');
+    const { email, password } = req.body;
+    res.send('login success');
 };
