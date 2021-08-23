36b3339a58e6fe72bf5f65a897c41a1fb5d242a9
Agus Janardana <bjanardana@gmail.com>
2021-08-23 13:51:13 +0800

    error handler

--- controller/authController.js
+++ controller/authController.js
@@ -1,9 +1,22 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 //error handling
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: '' };
+
+    // check kalau salah email
+    if (err.message.includes('salah email')) {
+        errors.email = 'Email belum teregistrasi';
+    }
+
+    // salah password
+
+    if (err.message.includes('Password Salah')) {
+        errors.password = 'Password kurang tepat';
+    }
 
     // duplicate email error
     if (err.code === 11000) {
@@ -23,6 +36,13 @@ const handleErrors = (err) => {
 
     return errors;
 };
+//Create jwt token, aku hanya cantumin id nya.
+const maxTime = 1 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: maxTime,
+    });
+};
 
 exports.register_get = (req, res) => {
     res.render('auth/register');
@@ -35,21 +55,33 @@ exports.login_get = (req, res) => {
 exports.register_post = async (req, res) => {
     const { email, password, name } = req.body;
 
-    // if (!req.body) {
-    //    res.status(400).send({
-    //       message: 'Ada field yang kosong',
-    //    });
-    // }
     try {
         const user = await User.create({ email, password, name });
-        res.status(200).json(user);
+        const token = createToken(user._id);
+        // dalam cookie, waktu itu harus ms
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxTime * 1000 });
+        res.status(200).json({ users: user._id });
     } catch (err) {
         const errors = handleErrors(err);
         res.status(400).json({ errors });
     }
 };
 
-exports.login_post = (req, res) => {
+exports.login_post = async (req, res) => {
     const { email, password } = req.body;
-    res.send('login success');
+
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxTime * 1000 });
+        res.status(200).json({ user: user._id });
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
+};
+
+exports.logout_get = (req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
 };
