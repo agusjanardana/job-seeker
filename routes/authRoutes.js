f0eb90ed34c10a1a54f7f10e00927672b98fe880
Agus Janardana <bjanardana@gmail.com>
2021-08-23 13:51:51 +0800

    fix route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 
 const authController = require('../controller/authController.js');
-
+const { requireAuth } = require('../middleware/authMiddleware');
 /** Disini adalah route untuk views
  * @method GET
  */
@@ -11,11 +11,12 @@ router.post('/registered', authController.register_post);
 
 router.get('/login', authController.login_get);
 router.post('/login', authController.login_post);
+router.get('/logout', authController.logout_get);
 
 router.get('/', (req, res) => {
     res.render('home');
 });
-router.get('/job', (req, res) => {
+router.get('/job', requireAuth, (req, res) => {
     res.render('job');
 });
 module.exports = router;
