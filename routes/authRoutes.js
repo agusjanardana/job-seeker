db4ca0e4fedd7cdecd3d8b9322181cc9bc409455
Agus Janardana <bjanardana@gmail.com>
2021-09-01 21:25:47 +0800

    friendly with postman

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,12 @@
 var router = require('express').Router();
-
+const jwt = require('jsonwebtoken');
 const authController = require('../controller/authController.js');
-const { requireAuth } = require('../middleware/authMiddleware');
+const { requireAuth, checkUser } = require('../middleware/authMiddleware');
+const User = require('../models/User');
 /** Disini adalah route untuk views
  * @method GET
  */
+// router.get('*', checkUser);
 
 router.get('/register', authController.register_get);
 router.post('/registered', authController.register_post);
@@ -13,9 +15,24 @@ router.get('/login', authController.login_get);
 router.post('/login', authController.login_post);
 router.get('/logout', authController.logout_get);
 
+router.post('/verify', async (req, res) => {
+    const { token } = req.body;
+    if (!token) {
+        return res.status(400).json({ status: 'error' });
+    }
+
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+    let user = await User.findById(decodedToken.id);
+    user.password = '';
+    return res.status(200).json({
+        data: user,
+    });
+});
+
 router.get('/', (req, res) => {
     res.render('home');
 });
+
 router.get('/job', requireAuth, (req, res) => {
     res.render('job');
 });
